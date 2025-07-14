939a0ba80cef2a00c8bdca1cfba5861b82f6d16a
codyzbizniz3 <codyzbizniz3@users.noreply.github.com>
2025-07-13 17:17:01 -0700

    Adds functionality to changeGrid and submit buttons.

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     const container = document.getElementById("container");
+    const changeGridBtn = document.getElementById("change-grid");
+    const shakeBtn = document.getElementById("shake-btn");
+    const overlay = document.getElementById("pop-up-overlay");
+    const popUp = document.getElementById("pop-up");
+    const cancelBtn = document.getElementById("cancel-btn");
+    const submitBtn = document.getElementById("submit-btn");
+    const inputField = document.getElementById("input-field");
     let isDrawing = false;
-    defaultGrid = 16;
-
-
-    // let userNum = prompt("Enter number of squares per side.");
-
-    // userNum = parseInt(userNum);
-    // if (isNaN(userNum) || userNum <= 0) {
-    //     userNum = 16;
-    //     alert("Invalid input. Defaulting to 16 x 16.");
-    // } else if (userNum > 100) {
-    //     userNum = 100;
-    //     alert("Number too large. Capping at 100 x 100.");
-    // }
+    defaultGrid = 57;
 
     function createGrid(num) {
         container.innerHTML = '';
-        container.style.setProperty('--grid-columns', defaultGrid);
+        container.style.setProperty('--grid-columns', num);
 
         const totalSquares = num * num;
         for (let i = 0; i < totalSquares; i++) {
@@ -29,6 +24,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
     createGrid(defaultGrid);
+
+
+
+    changeGridBtn.addEventListener('click', (e) => {
+        overlay.style.display = "block";
+        popUp.style.display = "block";
+        inputField.focus();
+    });
+
+    function closePopUp() {
+        overlay.style.display = "none";
+        popUp.style.display = "none";
+        inputField.value = "";
+
+    }
+
+    cancelBtn.addEventListener('click', closePopUp);
+
+    submitBtn.addEventListener('click', (e) => {
+        let userNum = inputField.value.trim();
+        if (isNaN(userNum) || userNum <= 0) {
+            userNum = 16;
+            alert("Invalid input. Defaulting to 16 x 16.");
+        } else if (userNum > 100) {
+            userNum = 100;
+            alert("Number too large. Capping at 100 x 100.");
+        } else {
+            createGrid(userNum);
+        }
+        closePopUp();
+    });
+
+
+
+
 
     container.addEventListener('mousedown', (e) => {
         isDrawing = true;
